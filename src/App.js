e264973f8f16f1e1b1b3c8ee8c1d0f19ae4aa639
anitta <anittavarghese@riafy.me>
2021-08-06 11:08:06 +0530

    user authentication using auth0

--- src/App.js
+++ src/App.js
@@ -1,134 +1,20 @@
-import "./App.css";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import ColorGame from "./components/ColorGame";
+import { useAuth0 } from "@auth0/auth0-react";
 
-export default function ColorGame() {
-  const [mode, setMode] = useState("hard");
-  const [numberOfSquares, setNumberOfSquares] = useState();
-  const [selectColor, setSelectColor] = useState();
-  const [arrayOfColors, setarrayOfColors] = useState([]);
-  const [gameMessage, setGameMessage] = useState("");
-  const [headingBg, setHeadingBg] = useState("steelblue");
-  const [buttonMessage, setButtonMessage] = useState("NEW COLORS");
+function App() {
+  const { isLoading } = useAuth0();
 
-  useEffect(() => {
-    setUpSquares();
-    reset();
-  }, [numberOfSquares]);
-
-  const setUpSquares = () => {
-    if (mode === "hard") {
-      setNumberOfSquares(6);
-    } else {
-      setNumberOfSquares(3);
-    }
-  };
-
-  const reset = () => {
-    setGameMessage("");
-    setButtonMessage("NEW COLORS");
-    const colors = generateRandomColors();
-    setarrayOfColors(colors);
-
-    const pickedColor = pickColor();
-
-    setSelectColor(colors[pickedColor]);
-  };
-
-  const pickColor = () => {
-    var random = Math.floor(Math.random() * numberOfSquares);
-    return random;
-  };
-
-  const generateRandomColors = () => {
-    var arr = [];
-    //repeat num times
-    console.log("squares", numberOfSquares);
-    for (var i = 0; i < numberOfSquares; i++) {
-      //get random color and push into arr
-      arr.push(randomColor());
-    }
-    //return that array
-    return arr;
-  };
-
-  function randomColor() {
-    //pick a "red" from 0 - 255
-    var r = Math.floor(Math.random() * 256);
-    //pick a "green" from  0 -255
-    var g = Math.floor(Math.random() * 256);
-    //pick a "blue" from  0 -255
-    var b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
-  }
-
-  const checkColor = (element, index) => {
-    if (element == selectColor) {
-      setGameMessage("Correct!");
-      const sameColors = Object.keys(arrayOfColors).map((index) => {
-        return selectColor;
-      });
-      setButtonMessage("PLAY AGAIN?");
-      setarrayOfColors(sameColors);
-      setHeadingBg(selectColor);
-    } else {
-      setGameMessage("Try Again ?");
-      const markWrongSelectedColor = Object.values(arrayOfColors).map(
-        (e, i) => {
-          if (i == index) {
-            return "rgb(" + 35 + ", " + 35 + ", " + 35 + ")";
-          } else {
-            return e;
-          }
-        }
-      );
-      setarrayOfColors(markWrongSelectedColor);
-    }
-  };
-
+  if (isLoading) return <div>Loading...</div>;
   return (
-    <div className="body">
-      <div className="heading" style={{ backgroundColor: `${headingBg}` }}>
-        <h2>The GREAT </h2>
-        <h1>{selectColor}</h1>
-        <h2>Color Game</h2>
-      </div>
-
-      <div className="menu-bar">
-        <button onClick={(e) => reset()}>{buttonMessage}</button>
-        &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-        {gameMessage}
-        &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
-        <button
-          onClick={(e) => {
-            setMode("easy");
-            setNumberOfSquares(3);
-            reset();
-          }}
-        >
-          EASY
-        </button>
-        <button
-          onClick={(e) => {
-            setMode("hard");
-            setNumberOfSquares(6);
-            reset();
-          }}
-        >
-          HARD
-        </button>
-      </div>
-      <p></p>
-      <div className="container">
-        {Object.values(arrayOfColors).map((element, index) => {
-          return (
-            <button
-              class="color-button"
-              style={{ backgroundColor: `${element}` }}
-              onClick={(e) => checkColor(element, index)}
-            ></button>
-          );
-        })}
-      </div>
+    <div>
+      <LoginButton></LoginButton>
+      <LogoutButton></LogoutButton>
+      <ColorGame></ColorGame>
     </div>
   );
 }
+
+export default App;
---
+++ src/components/ColorGame.js
@@ -0,0 +1,139 @@
+import "./ColorGame.css";
+import React, { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+export default function ColorGame() {
+  const {isAuthenticated } = useAuth0();
+  const [mode, setMode] = useState("hard");
+  const [numberOfSquares, setNumberOfSquares] = useState();
+  const [selectColor, setSelectColor] = useState();
+  const [arrayOfColors, setarrayOfColors] = useState([]);
+  const [gameMessage, setGameMessage] = useState("");
+  const [headingBg, setHeadingBg] = useState("steelblue");
+  const [buttonMessage, setButtonMessage] = useState("NEW COLORS");
+
+  useEffect(() => {
+    console.log(isAuthenticated);
+    setUpSquares();
+    reset();
+  }, [numberOfSquares]);
+
+  const setUpSquares = () => {
+    if (mode === "hard") {
+      setNumberOfSquares(6);
+    } else {
+      setNumberOfSquares(3);
+    }
+  };
+
+  const reset = () => {
+    setGameMessage("");
+    setButtonMessage("NEW COLORS");
+    const colors = generateRandomColors();
+    setarrayOfColors(colors);
+
+    const pickedColor = pickColor();
+
+    setSelectColor(colors[pickedColor]);
+  };
+
+  const pickColor = () => {
+    var random = Math.floor(Math.random() * numberOfSquares);
+    return random;
+  };
+
+  const generateRandomColors = () => {
+    var arr = [];
+    //repeat num times
+    console.log("squares", numberOfSquares);
+    for (var i = 0; i < numberOfSquares; i++) {
+      //get random color and push into arr
+      arr.push(randomColor());
+    }
+    //return that array
+    return arr;
+  };
+
+  function randomColor() {
+    //pick a "red" from 0 - 255
+    var r = Math.floor(Math.random() * 256);
+    //pick a "green" from  0 -255
+    var g = Math.floor(Math.random() * 256);
+    //pick a "blue" from  0 -255
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  }
+
+  const checkColor = (element, index) => {
+    if (element == selectColor) {
+      setGameMessage("Correct!");
+      const sameColors = Object.keys(arrayOfColors).map((index) => {
+        return selectColor;
+      });
+      setButtonMessage("PLAY AGAIN?");
+      setarrayOfColors(sameColors);
+      setHeadingBg(selectColor);
+    } else {
+      setGameMessage("Try Again ?");
+      const markWrongSelectedColor = Object.values(arrayOfColors).map(
+        (e, i) => {
+          if (i == index) {
+            return "rgb(" + 35 + ", " + 35 + ", " + 35 + ")";
+          } else {
+            return e;
+          }
+        }
+      );
+      setarrayOfColors(markWrongSelectedColor);
+    }
+  };
+
+  return (
+    isAuthenticated && (
+      <div className="body">
+        <div className="heading" style={{ backgroundColor: `${headingBg}` }}>
+          <h2>The GREAT </h2>
+          <h1>{selectColor}</h1>
+          <h2>Color Game</h2>
+        </div>
+
+        <div className="menu-bar">
+          <button onClick={(e) => reset()}>{buttonMessage}</button>
+          &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+          {gameMessage}
+          &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;&nbsp;
+          <button
+            onClick={(e) => {
+              setMode("easy");
+              setNumberOfSquares(3);
+              reset();
+            }}
+          >
+            EASY
+          </button>
+          <button
+            onClick={(e) => {
+              setMode("hard");
+              setNumberOfSquares(6);
+              reset();
+            }}
+          >
+            HARD
+          </button>
+        </div>
+        <p></p>
+        <div className="container">
+          {Object.values(arrayOfColors).map((element, index) => {
+            return (
+              <button
+                class="color-button"
+                style={{ backgroundColor: `${element}` }}
+                onClick={(e) => checkColor(element, index)}
+              ></button>
+            );
+          })}
+        </div>
+      </div>
+    )
+  );
+}
